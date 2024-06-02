484f37dfd06240af6a859ed7bac81d418832735a
Udham Singh <129770745+Udham7308@users.noreply.github.com>
2024-06-02 10:51:38 +0000

    do while loop

---
+++ iteration/while_loop.js
@@ -0,0 +1,19 @@
+
+let index  = 0;
+while (index<10) {
+    //console.log(`value of index is ${index}`);
+    index = index+2
+}
+
+let myArray = ['captain','docter','witch']
+let arr = 0
+while (arr<myArray.length) {
+    //console.log(`value is ${myArray[arr]}`);
+    arr = arr+1
+}
+
+let score =11
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score<10);
