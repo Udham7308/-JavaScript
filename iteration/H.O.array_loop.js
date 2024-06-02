72ddddb9d773861c4a8eea463b05b88c23adbf46
Udham Singh <129770745+Udham7308@users.noreply.github.com>
2024-06-02 11:23:11 +0000

    High order array loop

---
+++ iteration/H.O.array_loop.js
@@ -0,0 +1,39 @@
+/* Array Specific loops */
+// for of loop
+// for (const iterator of object) { // Here object is declear for structure(array,object ..etc).
+    
+// }
+
+const number = [1,2,3,4,5,6]
+
+for (const num of  number) {
+    //console.log(num);
+}
+
+const greetings = 'helloworld'
+
+for (const greet of greetings) {
+    //console.log(`Each char is ${greet}`);
+}
+
+/* Maps ==> Map objects are collections of key-value pairs. A key in the Map may only occur once; it is unique in the Map's collection */
+
+const map = new Map()
+map.set('IN', 'India')
+map.set('Fr',"France")
+map.set('UK',"united kingdom")
+map.set('IN', 'India')
+//console.log(map);
+
+for (const [key , value] of map) {
+    //console.log(key,':-',value);    
+}
+
+const myObject ={ // Here myObject is not iterable.
+    game1: 'NFS',
+    game2: 'COD'
+}
+for (const [key,value] of myObject) {
+    console.log(key,':-',value);
+}
+
